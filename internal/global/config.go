6499d690d7e9b3645575df0ac01517d0acb20bd7
huangbin1 <huangbin1@wps.cn>
2024-02-01 15:13:28 +0800

    rename [worker] to [consumer]

--- internal/global/config.go
+++ internal/global/config.go
@@ -15,11 +15,11 @@ type DispatcherConfig struct {
 	QueueName string
 }
 
-// WorkerConfig 消费者进程启动配置
-type WorkerConfig struct {
+// ConsumerConfig 消费者进程启动配置
+type ConsumerConfig struct {
 	RedisCfg
 	// 消息队列名字
 	QueueName string
-	// 每一个Worker节点启动多少个go routine用于处理Task
+	// 每一个Consumer节点启动多少个go routine用于处理Task
 	WorkerPoolSize int
 }
